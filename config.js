c7164a4107c5fd025df6642aef3aec2a3a9d98cc
alexandre839 <alexandre@sge.com.br>
2026-06-19 15:32:09 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ const CONFIG = {
     SPREADSHEET_ID: '1ckqTGbCzKB9W7MHKxSEQnY1tqjbd_KTFKGTVL8vT7cA',
 
     // Chave da API do Google Sheets
-    SHEETS_API_KEY: 'SUA_GOOGLE_SHEETS_API_KEY',
+    SHEETS_API_KEY: 'AIzaSyBRIPG7XUqOR-PYZ2PGifVWnQ4hDy7eDu0',
 
     // Site do Netlify (será preenchido após deploy)
-    NETLIFY_SITE_URL: 'AIzaSyBRIPG7XUqOR-PYZ2PGifVWnQ4hDy7eDu0',
+    NETLIFY_SITE_URL: 'https://sgedashboard.netlify.app/',
 
     // Tempo de cache em minutos
     CACHE_DURATION: 5
